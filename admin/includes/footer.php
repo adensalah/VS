749cc059f51e9e995b63669c4364da65b033fd3a
Adhan Moge Salah <Adhanedly@gmail.com>
2021-11-26 18:49:41 +0300

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+    <strong> &copy; 2021 Adensalah. All right reserved <a href="#">MKU</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2021. All right reserved<a href="#"> MKU OVS</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
